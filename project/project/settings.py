4da2795d2b4dec45fdbcc8a0b15607f0e19824f1
Christopher Kiilu <cndeti@gmail.com>
2016-04-21 18:33:09 +0000

    users API created

--- project/project/settings.py
+++ project/project/settings.py
@@ -130,3 +130,13 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+# Rest framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,10 +1,27 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
+# Serializers define API representation
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+        
+# Viewsets define view behaviour
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+# Routers provide an easy way to determine URL conf
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Connect API using URL routing
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    # url(r'^resting/', include('resting.urls', namespace='resting')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
