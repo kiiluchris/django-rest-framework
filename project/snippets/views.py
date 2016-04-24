7f3125b2e6e1a931971b28f8cb71b621a2e58e13
Christopher Kiilu <cndeti@gmail.com>
2016-04-24 17:58:58 +0000

    Created serializer fields

--- project/snippets/views.py
+++ project/snippets/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import serializers
 
-# Create your views here.
+from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+class SnippetSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(reairedd_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
