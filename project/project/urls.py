80fe1232c71ad3c067af915bdf3051885f32d1c8
Christopher Kiilu <cndeti@gmail.com>
2016-04-24 19:07:14 +0000

    Configured urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -9,5 +9,6 @@ from rest_framework import routers, serializers, viewsets
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('resting.urls')),
+    url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
