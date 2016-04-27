744577373bf00f8879c5c07a52f6a9669f445417
Christopher Kiilu <cndeti@gmail.com>
2016-04-27 17:37:03 +0000

    aDD snippet func

--- project/snippets/views.py
+++ project/snippets/views.py
@@ -15,6 +15,13 @@ class SnippetList(APIView):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
+        
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SnippetDetail(APIView):
     """
