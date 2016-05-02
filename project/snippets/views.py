81e71e353596529b272f23c7e39679d6353f0a05
Christopher Kiilu <cndeti@gmail.com>
2016-05-02 18:54:58 +0000

    User edit permissions

--- project/snippets/views.py
+++ project/snippets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions
 
@@ -21,7 +22,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
